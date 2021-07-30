bd77d01fa7d387cc531c620cfce4181380417062
DonTouzi <dontouzi@gmail.com>
2021-07-30 23:20:19 +0200

    Repo init

---
+++ README.md
@@ -0,0 +1,7 @@
+# TypeScript (TS)
+
+Getting started with TypeScript.
+
+TypeScript is a superset of JavaScript.
+
+- Project: Simple web application with TypeScript.
---
+++ greeter.js
@@ -0,0 +1,15 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName =
+            this.firstName + " " + this.middleInitial + " " + this.lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello, " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Walid", "D", "Touzi");
+document.body.textContent = greeter(user);
